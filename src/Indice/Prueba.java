a1391a88db361c7c3c397d1e0d8f1f01858d46c8
marromagu <mromagu2611@g.educaand.es>
2022-12-14 22:05:53 +0100

    Prueba_Bucles(DoWhile While for)

--- src/Indice/Prueba.java
+++ src/Indice/Prueba.java
@@ -15,6 +15,24 @@ public class Prueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int i, num;
+        num = 10;
+        for (i = 0; i <= num; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
+        i = 0;
+        while (i <= num) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println("");
+        i = 0;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= num);
+        System.out.println("");
     }
-    
+
 }
