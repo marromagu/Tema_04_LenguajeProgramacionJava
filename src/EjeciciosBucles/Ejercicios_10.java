fb9dd481389de4463c2707fbb4f4f82af4cea51a
marromagu <mromagu2611@g.educaand.es>
2023-01-18 13:03:07 +0100

    Ejecicios bucles fin

--- src/EjeciciosBucles/Ejercicios_10.java
+++ src/EjeciciosBucles/Ejercicios_10.java
@@ -46,6 +46,12 @@ public class Ejercicios_10 {
                 case 8:
                     ejercicio8(teclado);
                     break;
+                case 9:
+                    ejercicio9(teclado);
+                    break;
+                case 10:
+                    ejercicio10(teclado);
+                    break;
                 default://Nada que hacer
             }
             if (op != 0) {
@@ -71,6 +77,8 @@ public class Ejercicios_10 {
         System.out.println("6.- Ejercicio 6");
         System.out.println("7.- Ejercicio 7");
         System.out.println("8.- Ejercicio 8");
+        System.out.println("9.- Ejercicio 9");
+        System.out.println("10.- Ejercicio 10");
         System.out.println();
         System.out.print("Introduzca opción: ");
         int op = Integer.parseInt(a.nextLine());
@@ -91,6 +99,7 @@ public class Ejercicios_10 {
             System.out.print(i + " ");
         }
         System.out.println();
+        System.out.println("-----------");
     }
 
     public static void ejercicio2(Scanner te) {
@@ -108,6 +117,7 @@ public class Ejercicios_10 {
             System.out.print(i + " ");
         }
         System.out.println(" ");
+        System.out.println("-----------");
     }
 
     public static void ejercicio3(Scanner tec) {
@@ -124,7 +134,9 @@ public class Ejercicios_10 {
 
             suma = suma + i;
         }
+        System.out.println("-----------");
         System.out.println(suma);
+        System.out.println("-----------");
     }
 
     public static void ejercicio4(Scanner tecl) {
@@ -146,6 +158,7 @@ public class Ejercicios_10 {
             }
         }
         System.out.println("");
+        System.out.println("-----------");
 
     }
 
@@ -167,8 +180,9 @@ public class Ejercicios_10 {
                 suma = suma + i;
             }
         }
+        System.out.println("-----------");
         System.out.println(suma);
-        System.out.println("");
+        System.out.println("-----------");
     }
 
     public static void ejercicio6(Scanner teclad) {
@@ -183,10 +197,12 @@ public class Ejercicios_10 {
             if (num > 0) {
                 System.out.println("Es positivo");
                 System.out.println("El proceso se repetirá hasta que se introduzca un 0.");
+                System.out.println("-----------");
             }
             if (num < 0) {
                 System.out.println("Es negativo");
                 System.out.println("El proceso se repetirá hasta que se introduzca un 0.");
+                System.out.println("-----------");
             }
 
         } while (num != 0);
@@ -207,7 +223,9 @@ public class Ejercicios_10 {
             }
 
         } while (num > 0);
+        System.out.println("-----------");
         System.out.println(suma);
+        System.out.println("-----------");
     }
 
     public static void ejercicio8(Scanner t) {
@@ -222,6 +240,54 @@ public class Ejercicios_10 {
             System.out.println("Adivina el numero secreto");
             num = Integer.parseInt(t.nextLine());
         } while (num != numSecreto);
+        System.out.println("-----------");
         System.out.println("¡¡¡¡BIEN!!!!");
+        System.out.println("-----------");
+    }
+
+    public static void ejercicio9(Scanner t) {
+        /*Pedir 10 números y escribir la suma total.*/
+        System.out.println("Ejercicio 9");
+        System.out.println("-----------");
+        System.out.println();
+        System.out.println("Escribe 10 numeros");
+        int suma = 0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Numero " + i);
+            int num = t.nextInt();
+            suma = suma + num;
+        }
+        System.out.println("-----------");
+        System.out.println(suma);
+        System.out.println("-----------");
+    }
+
+    public static void ejercicio10(Scanner t) {
+        /*Pedir un número N, introducir N números y mostrar el mínimo, máximo y la media*/
+        System.out.println("Ejercicio 10");
+        System.out.println("-----------");
+        System.out.println();
+        System.out.println("Escribe N");
+        int N = t.nextInt();
+        int max = 0;
+        int min = 999999;
+        float suma = 0;
+        for (int i = 0; i < N; i++) {
+            System.out.println("Escribe un nuemro");
+            int num = t.nextInt();
+            if (num > max) {
+                max = num;
+            }
+            if (num < min) {
+                min = num;
+            }
+            suma = suma + num;
+
+        }
+        System.out.println("-----------");
+        System.out.println("maximo " + max);
+        System.out.println("minimo " + min);
+        System.out.println("media " + (suma / N));
+        System.out.println("-----------");
     }
 }
